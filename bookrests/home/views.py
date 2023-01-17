65e2a8d581ac8f6c7596291e7c9ef1d61f0e1a80
AkanshVerma <vermaakansh7@gmail.com>
2023-01-17 13:48:00 +0530

    Completed base website

--- bookrests/home/views.py
+++ bookrests/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -6,6 +9,7 @@ def index(request):
         'variable1': "Hello, world",
         'variable2': "World, Hello"
     }
+    
     return render(request, 'index.html', context)
     # return HttpResponse("this is homepage")
 
@@ -20,7 +24,10 @@ def services(request):
 def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        
-        
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, desc=desc,date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent! ')
     return render(request, 'contact.html')
     #return HttpResponse("This is contact page")
